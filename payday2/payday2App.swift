03580c83125c4dd320a6f1c8e4296963abe01111
alex-meinecke <alexandermeinecke@icloud.com>
2024-08-17 11:16:37 -0700

    Initial Commit

---
+++ payday2/payday2App.swift
@@ -0,0 +1,20 @@
+//
+//  payday2App.swift
+//  payday2
+//
+//  Created by Meinecke, Alexander on 17.08.24.
+//
+
+import SwiftUI
+
+@main
+struct payday2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ payday2/view/TripView.swift
@@ -0,0 +1,18 @@
+//
+//  TripView.swift
+//  payday2
+//
+//  Created by Meinecke, Alexander on 17.08.24.
+//
+
+import SwiftUI
+
+struct TripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripView()
+}
